ee4882a2640a4561110b60c42c8d33b22aac0e52
Everson Silva <everson_gsilva@yahoo.com.br>
2021-10-27 15:23:49 -0300

    chore: Files project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
+const App = () => {
+
+  return (
+    <Router>
+      <GlobalStyle />
+      <Routes />
+    </Router>
+  )
+}
+
+export default App
