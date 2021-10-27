085a922f4e0619c3a51747e460c89d411827423a
Everson Silva <everson_gsilva@yahoo.com.br>
2021-10-27 15:20:23 -0300

    styles: styled global

---
+++ src/styles/global.js
@@ -0,0 +1,68 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+* {
+  margin: 0%;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+ :root {
+   --white: #FFF;
+
+   --gray-50: #F7F8FA;
+   --gray-100: #E6E8EB;
+   --gray-200: #AFB2B1;
+   --gray-500: #808080;
+   --gray-800: #494D4B;
+
+   --green-500: #04D361;
+
+   --purple-300: #9F75FF;
+   --purple-400: #9164FA;
+   --purple-500: #8257E5;
+   --purple-800: #6F48C9;
+ }
+
+ @media (max-width: 1080px){
+   html {
+     font-size: 93.75%;
+   }
+ }
+
+ @media (max-width: 720px){
+   html {
+     font-size: 87.5%;
+   }
+ }
+
+ s:focus {
+   outline: 0;
+ }
+
+
+body {
+  background: '--gray-100';
+  -webkit-font-smoothing: antialiased;
+}
+
+body, input, button{
+  font-family: 'Roboto', serif;
+  font-size: 16px;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-weight: 600;
+}
+button {
+  cursor: pointer;
+}
+
+a {
+  text-decoration: none;
+}
+ul {
+  list-style: none;
+}`;
